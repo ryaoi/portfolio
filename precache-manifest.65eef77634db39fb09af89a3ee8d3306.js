e705b63c903a50ccd0fc48854390689be1cdeb46
ryaoi <ryaoi@student.42.fr>
2019-04-22 22:27:30 +0200

    Updates

---
+++ precache-manifest.65eef77634db39fb09af89a3ee8d3306.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "304afa675925f447ebe9",
+    "url": "/portfolio/static/css/main.8c4ccf39.chunk.css"
+  },
+  {
+    "revision": "304afa675925f447ebe9",
+    "url": "/portfolio/static/js/main.ef966892.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "835e4645878011234435",
+    "url": "/portfolio/static/js/2.c5b45f8a.chunk.js"
+  },
+  {
+    "revision": "025256ab4ce38909010d6ffbcab5d663",
+    "url": "/portfolio/index.html"
+  }
+];
